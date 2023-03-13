23d0cb6fbfece6223d8bfdbb6e93177a3bfeecad
pdheeru <100355011+pdheeru@users.noreply.github.com>
2023-03-13 22:20:30 +0530

    Create instance-module-01-variable.tf

---
+++ instance-module-01-variable.tf
@@ -0,0 +1,91 @@
+variable "tags" {
+  type        = list(string)
+  description = "Network tags, provided as a list"
+  default     = []
+}
+
+variable "labels" {
+  type        = map(string)
+  description = "Labels, provided as a map"
+  default     = {}
+}
+
+variable "machine_type" {
+type = string
+}
+
+variable "hostname" {
+  type = string
+}
+
+variable "zone" {
+  type        = string
+  description = "Zone where the instances should be created. If not specified, instances will be spread across available zones in the region."
+  default     = null
+}
+
+
+
+####################
+# network_interface
+####################
+variable "network" {
+  description = "The name or self_link of the network to attach this interface to. Use network attribute for Legacy or Auto subnetted networks and subnetwork for custom subnetted networks."
+  default     = ""
+}
+
+variable "subnetwork" {
+  description = "The name of the subnetwork to attach this interface to. The subnetwork must exist in the same region this instance will be created in. Either network or subnetwork must be provided."
+  default     = ""
+}
+
+variable "subnetwork_project" {
+  description = "The ID of the project in which the subnetwork belongs. If it is not provided, the provider project is used."
+  default     = ""
+}
+
+variable "network_ip" {
+  description = "Private IP address to assign to the instance if desired."
+  default     = ""
+}
+
+variable "additional_networks" {
+  description = "Additional network interface details for GCE, if any."
+  default     = []
+  type = list(object({
+    network            = string
+    subnetwork         = string
+    subnetwork_project = string
+    network_ip         = string
+    access_config = list(object({
+      nat_ip       = string
+      network_tier = string
+    }))
+  }))
+}
+###########################
+# Public IP
+###########################
+variable "access_config" {
+  description = "Access configurations, i.e. IPs via which the VM instance can be accessed via the Internet."
+  type = list(object({
+    nat_ip       = string
+    network_tier = string
+  }))
+  default = []
+}
+##################
+# alias IP range
+##################
+variable "alias_ip_range" {
+  description = <<EOF
+An array of alias IP ranges for this network interface. Can only be specified for network interfaces on subnet-mode networks.
+ip_cidr_range: The IP CIDR range represented by this alias IP range. This IP CIDR range must belong to the specified subnetwork and cannot contain IP addresses reserved by system or used by other network interfaces. At the time of writing only a netmask (e.g. /24) may be supplied, with a CIDR format resulting in an API error.
+subnetwork_range_name: The subnetwork secondary range name specifying the secondary range from which to allocate the IP CIDR range for this alias IP range. If left unspecified, the primary range of the subnetwork will be used.
+EOF
+  type = object({
+    ip_cidr_range         = string
+    subnetwork_range_name = string
+  })
+  default = null
+}
