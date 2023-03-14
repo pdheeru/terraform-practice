266eaefe5688247bf774f67c05c07fb264259bc6
Dheeraj <pdheeru0@gmail.com>
2023-03-14 07:09:50 +0000

    module files added

--- modules/instance-module-01-variable.tf
+++ modules/instance-module-01-variable.tf
@@ -1,4 +1,4 @@
-variable "tags" {
+" {
   type        = list(string)
   description = "Network tags, provided as a list"
   default     = []
@@ -23,7 +23,6 @@ variable "zone" {
   description = "Zone where the instances should be created. If not specified, instances will be spread across available zones in the region."
   default     = null
 }
-
 
 
 ####################
